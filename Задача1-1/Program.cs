06b27e2964b1ab2b5cc8a5f1075f8500fa6516e6
Valentin <zvezdochet.79@mail.ru>
2022-03-09 16:20:47 +0300

    Задача 1-1

---
+++ Задача1-1/Program.cs
@@ -0,0 +1,29 @@
+﻿//  Напишите программу, которая принимает на вход пятизначное число 
+//и проверяет, является ли оно палиндромом.
+
+
+Console.Write("Введите число:");
+string n = Console.ReadLine();
+string str = n.ToString();
+int[] array = new int[str.Length];
+int j = 0;
+while(j < str.Length)
+{
+    array[j] = int.Parse(str[j].ToString());
+    j++;
+}
+
+
+int i = 0;
+if(str.Length < 2) Console.WriteLine("Число должно иметь хотя бы два разряда");
+while (i < str.Length/2)
+{
+    if (array[i] != array[str.Length - i - 1])
+        {
+        Console.WriteLine("no");
+        break;
+       
+        }
+    i++;
+    if(i == str.Length/2) Console.WriteLine("yes"); 
+}
